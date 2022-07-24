88a75abcf18e94dd5d48d08f9bf9bb22716b28cb
NwaObed <Obed.Nwachukwu@warwick.ac.uk>
2022-07-24 04:42:16 +0100

    dummy

---
+++ price_compare/base/urls.py
@@ -0,0 +1,10 @@
+from unicodedata import name
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path(
+        'dummy', views.dummy_page,
+        name = 'dummy_page'
+    )
+]
--- price_compare/base/views.py
+++ price_compare/base/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def dummy_page(request):
+     return render(
+         request,
+         'dummy.html'
+     )

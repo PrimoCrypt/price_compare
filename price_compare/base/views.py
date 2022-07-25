447e5a2268b5e65593b03f1faa15bd7e4c256f36
mirera <mirera.moses@gmail.com>
2022-07-25 10:00:50 +0300

    Added LoginView and LogoutView Functionality

--- price_compare/base/views.py
+++ price_compare/base/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-def dummy_page(request):
-     return render(
-         request,
-         'dummy.html'
-     )
+
+#homepage view
+def homePage(request):
+
+    return render(request, 'base/home.html')
+
+
+
